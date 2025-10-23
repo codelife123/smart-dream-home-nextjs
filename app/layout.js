22468c133577f130b693bf9d20ee7e6f86b29908
codelife123 <nodedevloper@gmail.com>
2025-10-23 20:30:12 +0530

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
   title: "Smart Dream Home — Shop",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="si">
       <body>
         {children}
+		<Analytics />
       </body>
     </html>
   );
